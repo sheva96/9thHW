d10ce8ea0596e01747a426d8e189eb3fe2de35fc
Yevhen Shevchenko <sheva9620@gmail.com>
2020-11-11 11:24:56 +0200

    Initial Commit

---
+++ 9thHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9thHW
+//
+//  Created by Евгений Шевченко on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
